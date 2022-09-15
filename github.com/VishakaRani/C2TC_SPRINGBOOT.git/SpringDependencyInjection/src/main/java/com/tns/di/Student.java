b1c0e7a3f4addd2ab01ab1c093a1760567ef6cd6
VISHAKA RANI <VISHAKA RANI@user>
2022-09-15 23:48:34 +0530

    first commit

---
+++ github.com/VishakaRani/C2TC_SPRINGBOOT.git/SpringDependencyInjection/src/main/java/com/tns/di/Student.java
@@ -0,0 +1,17 @@
+package com.tns.di;
+
+public class Student {
+	private String studentName;
+    private int id;
+   
+	public Student(String studentName, int id) {
+		this.studentName = studentName;
+		this.id = id;
+	}
+
+	public void display()
+	{
+		System.out.println("Student data is "+studentName+" "+id);
+	}
+	
+}
