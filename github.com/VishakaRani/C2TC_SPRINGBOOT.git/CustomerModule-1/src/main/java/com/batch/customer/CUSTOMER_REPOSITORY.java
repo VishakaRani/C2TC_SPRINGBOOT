767785c5ae75732a1dfeae36cf6df456b735228b
Vish <106138418+VishakaRani@users.noreply.github.com>
2022-09-29 19:39:57 +0530

    Delete github.com/VishakaRani/C2TC_SPRINGBOOT.git/CustomerModule-1 directory

--- github.com/VishakaRani/C2TC_SPRINGBOOT.git/CustomerModule-1/src/main/java/com/batch/customer/CUSTOMER_REPOSITORY.java
+++
@@ -1,9 +0,0 @@
-package com.batch.customer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface CUSTOMER_REPOSITORY extends JpaRepository<CUSTOMER, Integer> 
-{
-   
-}
